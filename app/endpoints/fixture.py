c1e514a0d724dad4a85eeb5c6fec77146ac555d8
Krzysztof Bober <krzysztof.bober@getbase.com>
2018-09-24 17:06:17 +0200

    initial commit

---
+++ app/endpoints/fixture.py
@@ -0,0 +1,40 @@
+from datetime import datetime
+
+from ibet.app.endpoints.match import Match
+
+
+def get_timestamp():
+    return datetime.now().strftime(("%Y-%m-%d %H:%M"))
+
+
+FIXTURES = [
+    {
+        "id": 1,
+        "home_team": "FC Liverpool",
+        "away_team": "Chelsea",
+        "date": "2018-09-29 16:00",
+    },
+    {
+        "id": 2,
+        "home_team": "Arsenal",
+        "away_team": "Manchester City",
+        "date": "2018-09-29 18:00"
+    },
+    {
+        "id": 3,
+        "home_team": "West Ham United",
+        "away_team": "Manchester United",
+        "date": "2018-09-30 15:00"
+    }
+]
+
+
+def read():
+    return FIXTURES
+
+
+class Fixture(Match):
+    def __init__(self, home_team, away_team, date):
+        super().__init__(self, home_team, away_team, date)
+
+
---
+++ app/endpoints/match.py
@@ -0,0 +1,4 @@
+class Match:
+    def __init__(self, home_team, away_team, date):
+        self.home_team, away_team, date,  = home_team, away_team, date
+
